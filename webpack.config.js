87bd4e27a10be033ee69a0d26f66f3d9f8da431d
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-01-18 15:12:38 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+ export default {
+   module: {
+     rules: [
+       {
+         test: /\.less$/,
+         use: [
+           "style-loader",
+           {
+             loader: "css-loader",
+             options: { sourceMap: 1 }
+           },
+           "less-loader"
+         ]
+       }
+     ]
+   }
+ };
+
+ 
