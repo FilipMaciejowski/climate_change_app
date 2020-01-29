bd668c6c0d4bf2818e4ca076f165224221921c01
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-01-29 18:50:12 +0100

    map full-size

--- src/components/mapRender/index.js
+++ src/components/mapRender/index.js
@@ -1,45 +1,60 @@
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
 import React from "react";
 
-const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+import { Select } from 'antd';
+import 'antd/dist/antd.css';
+const { Option } = Select;
 
-const MapRender = ({ setTooltipContent, openForm }) => {
+const MapRender = ({ setTooltipContent, openForm, geoUrl, countryNames }) => {
+
+  const options = [];
+  countryNames.forEach((name, index) => {
+    options.push(<Option key={name + index}>{name}</Option>);
+  });
+
   return (
-    <ComposableMap data-tip="" projectionConfig={{ scale: 150 }}>
-      <ZoomableGroup>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                onMouseEnter={() => {
-                  const { NAME } = geo.properties;
-                  setTooltipContent(`${NAME}`);
-                }}
-                onMouseLeave={() => { setTooltipContent("") }}
-                onClick={() => openForm(geo.properties.NAME, geo.properties.ISO_A2, geo.properties.ISO_A3)
-                }
-                style={{
-                  default: {
-                    fill: "#D6D6DA",
-                    outline: "none"
-                  },
-                  hover: {
-                    fill: "#F53",
-                    outline: "none"
-                  },
-                  pressed: {
-                    fill: "#E42",
-                    outline: "none"
+    <div className='map__wrapper'>
+      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
+        <ZoomableGroup>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { NAME } = geo.properties;
+                    setTooltipContent(`${NAME}`);
+                  }}
+                  onMouseLeave={() => { setTooltipContent("") }}
+                  onClick={() => openForm(geo.properties.NAME, geo.properties.ISO_A2, geo.properties.ISO_A3)
                   }
-                }}
-              />
-            ))
-          }
-        </Geographies>
-      </ZoomableGroup>
-    </ComposableMap>
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none"
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none"
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none"
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+      <div className='search-input'>
+        <Select mode="tags"  tokenSeparators={[',']}>
+          {options}
+        </Select>
+      </div>
+    </div>
   )
 };
 
--- src/containers/map/index.js
+++ src/containers/map/index.js
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import ReactTooltip from 'react-tooltip';
 import { addCountry, fetchClimateData, fetchCountryData, fetchImages } from '../../redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import MapRender from "../../components/mapRender";
 import CountryInfoModal from "../../components/countryInfoModal";
 import './style.scss';
+import data from '../../constans/mapData'
+
+const geoUrl = data;
+
 
 const MapPage = () => {
   const dispatch = useDispatch();
   const [formOpen, setFormOpen] = useState(false);
   const [content, setContent] = useState("");
+  const [countryNames, setCountryNames] = useState([]);
 
   const { location, status, country } = useSelector(
     (state) => state.location
   );
+
+  useEffect(() => {
+    setCountryNames(getCountryNames(geoUrl));
+  }, []);
 
   const openForm = (countryName, countryNameShorthand, countryNameIso3) => {
     dispatch(addCountry(countryNameShorthand, countryName, countryNameIso3));
@@ -47,13 +56,24 @@ const MapPage = () => {
     return arrayURL;
   };
 
+  const getCountryNames = (mapData) => {
+    const namesArray = [];
+    mapData.objects.ne_110m_admin_0_countries.geometries.forEach(countryData => {
+      namesArray.push(countryData.properties.NAME)
+    });
+    return namesArray;
+  };
+
   return (
       <div className="map__main">
-        
-        <MapRender
-          setTooltipContent={setContent}
-          openForm={openForm}
-        />
+        {countryNames.length > 0 &&
+          <MapRender
+            setTooltipContent={setContent}
+            openForm={openForm}
+            geoUrl={geoUrl}
+            countryNames={countryNames}
+          />
+        }
         {formOpen === true &&
           <CountryInfoModal
             formOpen={formOpen}
