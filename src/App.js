8ab91b1f26bfe312f9b654e13349456d27327b7d
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-10 14:42:23 +0100

    router change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Story from "./containers/story/index";
 
 const App = () => {
   return (
-    <BrowserRouter basename={"/React"}>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/story" component={Story} />
