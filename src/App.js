e685adee2bb4925168466e02cc954cad9c155fb5
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-10 19:07:41 +0100

    basename 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import store from "./redux/store";
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
+      <Router basename={window.window.location.pathname || ''}>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/story" component={Story} />
