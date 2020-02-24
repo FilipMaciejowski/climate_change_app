8020671f3c4513a3056ccff247df28fbb3e3a110
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-24 20:28:13 +0100

    Updates

---
+++ precache-manifest.32470d864f80a60ac88ed5d38d0b3d36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8d0a7247da5fc612bf116b30b6e8bc",
+    "url": "/climate_change_app/index.html"
+  },
+  {
+    "revision": "05feb1f0facc6e050b70",
+    "url": "/climate_change_app/static/css/2.4c9c5ea8.chunk.css"
+  },
+  {
+    "revision": "aad951b7db203086b6cb",
+    "url": "/climate_change_app/static/css/main.5257a5e2.chunk.css"
+  },
+  {
+    "revision": "05feb1f0facc6e050b70",
+    "url": "/climate_change_app/static/js/2.26cdc7e4.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/climate_change_app/static/js/2.26cdc7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad951b7db203086b6cb",
+    "url": "/climate_change_app/static/js/main.af3e669e.chunk.js"
+  },
+  {
+    "revision": "9aab2622990e714d7c25",
+    "url": "/climate_change_app/static/js/runtime-main.4f9e3b37.js"
+  },
+  {
+    "revision": "71c19fce476c68fd7907a5bb74a6d882",
+    "url": "/climate_change_app/static/media/bg-gradient-wave.71c19fce.png"
+  },
+  {
+    "revision": "344ee6eaad74df6b72dec90b1b888aab",
+    "url": "/climate_change_app/static/media/lato-v16-latin_latin-ext-regular.344ee6ea.woff2"
+  },
+  {
+    "revision": "4f3c1d7c13ca26bceb5392edfbba5cea",
+    "url": "/climate_change_app/static/media/lato-v16-latin_latin-ext-regular.4f3c1d7c.eot"
+  },
+  {
+    "revision": "9087e4a6aceecc9b2914823044951a3a",
+    "url": "/climate_change_app/static/media/lato-v16-latin_latin-ext-regular.9087e4a6.svg"
+  },
+  {
+    "revision": "92b92a023bbe61753d9d2dfebf76fb2b",
+    "url": "/climate_change_app/static/media/lato-v16-latin_latin-ext-regular.92b92a02.ttf"
+  },
+  {
+    "revision": "a53df66f339b35b6a9b18b41980d0005",
+    "url": "/climate_change_app/static/media/lato-v16-latin_latin-ext-regular.a53df66f.woff"
+  },
+  {
+    "revision": "d062787c64f1e608847aa7efe627d75e",
+    "url": "/climate_change_app/static/media/wave-darkmode.d062787c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd97d1f2230633a9364b8b895a5b1389.js"
+  "/climate_change_app/precache-manifest.32470d864f80a60ac88ed5d38d0b3d36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/climate_change_app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
