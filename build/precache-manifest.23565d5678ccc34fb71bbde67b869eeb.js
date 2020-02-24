be69cea3e053c198d49f12eefbbdf65ddd1daacf
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-24 20:12:08 +0100

    static

---
+++ build/precache-manifest.23565d5678ccc34fb71bbde67b869eeb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a05a532a1519bf8eb274f5324374c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c149204e50ff7bae43",
+    "url": "/static/css/2.4c9c5ea8.chunk.css"
+  },
+  {
+    "revision": "cb905d68bbd1ad6fb81e",
+    "url": "/static/css/main.c3b4959d.chunk.css"
+  },
+  {
+    "revision": "b8c149204e50ff7bae43",
+    "url": "/static/js/2.1e19c5ba.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/static/js/2.1e19c5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb905d68bbd1ad6fb81e",
+    "url": "/static/js/main.af3e669e.chunk.js"
+  },
+  {
+    "revision": "e7bd21a59b68019eee62",
+    "url": "/static/js/runtime-main.b6dcde93.js"
+  },
+  {
+    "revision": "71c19fce476c68fd7907a5bb74a6d882",
+    "url": "/static/media/bg-gradient-wave.71c19fce.png"
+  },
+  {
+    "revision": "344ee6eaad74df6b72dec90b1b888aab",
+    "url": "/static/media/lato-v16-latin_latin-ext-regular.344ee6ea.woff2"
+  },
+  {
+    "revision": "4f3c1d7c13ca26bceb5392edfbba5cea",
+    "url": "/static/media/lato-v16-latin_latin-ext-regular.4f3c1d7c.eot"
+  },
+  {
+    "revision": "9087e4a6aceecc9b2914823044951a3a",
+    "url": "/static/media/lato-v16-latin_latin-ext-regular.9087e4a6.svg"
+  },
+  {
+    "revision": "92b92a023bbe61753d9d2dfebf76fb2b",
+    "url": "/static/media/lato-v16-latin_latin-ext-regular.92b92a02.ttf"
+  },
+  {
+    "revision": "a53df66f339b35b6a9b18b41980d0005",
+    "url": "/static/media/lato-v16-latin_latin-ext-regular.a53df66f.woff"
+  },
+  {
+    "revision": "d062787c64f1e608847aa7efe627d75e",
+    "url": "/static/media/wave-darkmode.d062787c.png"
+  }
+]);
